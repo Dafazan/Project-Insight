56adc044df6742dbdbb81ee269c66ddfb313daf0
Daffa Fauzan <dafazanlbs@gmail.com>
2023-11-05 15:46:38 +0700

    change layout

---
+++ src/components/Layouts/mobile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MobileLayout({ children }) {
+    return (
+        <div className='p-5 flex justify-center items-center'>
+            {children}
+
+        </div>
+    )
+}
+
+export default MobileLayout
