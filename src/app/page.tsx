2ce1b3e3c98c2db8db1a2a5cf9c5ea98f40e2b21
Daffa Fauzan <dafazanlbs@gmail.com>
2023-11-18 17:42:13 +0700

    dash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,44 @@ import { auth } from "../firebase";
 import { useRouter } from "next/navigation";
 import { redirect } from "next/navigation";
 import { onAuthStateChanged } from "firebase/auth";
+import MobileLayout from "@/components/Layouts/mobile"
+import DesktopLayout from "@/components/Layouts/desktop"
+import { isMobile } from 'react-device-detect';
 
+function Page() {
+  const [isLoginSuceed, setIsLoginSuceed] = useState(false);
+  const { push } = useRouter();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        setIsLoginSuceed(true);
+
+        // ...
+      } else {
+        push("/LoginPage");
+        // User is signed out
+        // ...
+      }
+    });
+  }, []);
+  return (
+
+    <>
+      <button
+
+        onClick={() => {
+          signOut(auth);
+          localStorage.removeItem("auth");
+          localStorage.clear();
+          push("/LoginPage");
+        }}
+      >Logout</button>
+    </>
+  )
+}
 
 export default function Home() {
   const [isLoginSuceed, setIsLoginSuceed] = useState(false);
@@ -30,20 +67,31 @@ export default function Home() {
     });
   }, []);
 
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+
   return (
     <>
       {isLoginSuceed ? (
         <>
-          <p>Welcome my friend, Still nothing huh</p>
-          <button
+          {isClient && (
+            <>
+              {isMobile ? (
+                <MobileLayout>
+                  <Page />
+                </MobileLayout>
+              ) : (
+                <DesktopLayout>
+                  <Page />
+                </DesktopLayout>
+              )}
+            </>
+          )}
 
-            onClick={() => {
-              signOut(auth);
-              localStorage.removeItem("auth");
-              localStorage.clear();
-              push("/LoginPage");
-            }}
-          >Logout</button>
         </>
       ) : null}
     </>
