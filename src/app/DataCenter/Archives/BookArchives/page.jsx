76b50e8647b7d827e4b51e77db1f76d2b6e3cf2c
Daffa Fauzan <dafazanlbs@gmail.com>
2024-01-31 15:03:22 +0700

    muvi

--- src/app/DataCenter/Archives/BookArchives/page.jsx
+++ src/app/DataCenter/Archives/BookArchives/page.jsx
@@ -1,8 +1,110 @@
+'use client'
 import React from 'react'
+import TitleStatusCard from "@/components/cards/TitleStatusCard"
+import CardsCanvas from "@/components/Layouts/CardsCanvas"
+import {
+    collection,
+    addDoc,
+    getDocs,
+    orderBy,
+    deleteDoc,
+    query,
+    where,
+    Firestore,
+    getDoc,
+    updateDoc,
+    doc,
+    increment,
+    limit,
+    startAfter
+} from "firebase/firestore";
+import { db, storage } from "../../../../firebase-p";
+import { auth } from "../../../../firebase";
+import { useEffect, useState } from "react";
+import MobileInnerLayout from '@/components/Layouts/MobileInnerLayout'
+import { useRouter } from "next/navigation";
+import { onAuthStateChanged } from "firebase/auth";
+import { isMobile } from 'react-device-detect'
+import AppName from '@/components/Layouts/AppName'
+import SideButton from '@/components/Buttons/SideButton'
+import SideLink from '@/components/Buttons/SideLink'
+import DesktopLayout from "@/components/Layouts/desktop"
 
 function BookArchives() {
+    const [dataBooks, setDataBooks] = useState([]);
+    useEffect(() => {
+        getDataBooks();
+    }, []);
+
+    async function getDataBooks() {
+        try {
+            const ordersRef = collection(db, "books");
+            const q = query(ordersRef, orderBy("title"));
+            const querySnapshot = await getDocs(q);
+
+            let data = [];
+            querySnapshot.forEach((doc) => {
+                data.push({ ...doc.data(), id: doc.id });
+            });
+
+            setDataBooks(data);
+            console.log('Fetched data:', data);
+
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
     return (
-        <div>BookArchives</div>
+        <>
+            <div className='h-screen w-screen overflow-y-scroll'>
+                <p>Record Found: {dataBooks.length} Entry</p>
+                {dataBooks.length === 0 ? (
+                    <p>No books found</p>
+                ) : (
+                    dataBooks.map((data, i) => (
+                        <div key={i} className='pb-5 flex w-[50%] justify-between'>
+                            <div>
+                                <p>{data.title}</p>
+                                <p>ISBN: {data.isbn}</p>
+                                <p>Author: {data.author}</p>
+                                <p>Published By: {data.publisher}, {data.year}</p>
+                                <p>Pages: {data.pages}</p>
+                                <p>type: {data.booktype}</p>
+                                <p>ID: {data.id}</p>
+
+                            </div>
+                            <div><button
+                                onClick={async (e) => {
+                                    const docRef = doc(db, 'books', data.id);
+
+                                    try {
+                                        await updateDoc(docRef, { booktype: 'fiction' }); // Update document with new field
+                                        alert("Update successful");
+                                        console.log("Document updated successfully");
+                                    } catch (error) {
+                                        console.error("An error occurred:", error);
+                                    }
+                                }}
+                                className='border-2 border-b-0 border-blue-500 px-2 text-green-500'>FICTION</button></div>
+                            <div><button
+                                onClick={async (e) => {
+                                    const docRef = doc(db, 'books', data.id);
+
+                                    try {
+                                        await updateDoc(docRef, { booktype: 'nonfiction' }); // Update document with new field
+                                        alert("Update successful");
+                                        console.log("Document updated successfully");
+                                    } catch (error) {
+                                        console.error("An error occurred:", error);
+                                    }
+                                }}
+                                className='border-2 border-b-0 border-blue-500 px-2 text-green-500'>NONFIC</button></div>
+                        </div>
+                    ))
+                )}
+            </div>
+
+        </>
     )
 }
 
--- src/app/Playground/page.jsx
+++ src/app/Playground/page.jsx
@@ -1,6 +1,8 @@
 'use client'
+import axios from 'axios';
 import React from 'react'
 import { useEffect, useState } from "react";
+import ButtonDefault from '@/components/Buttons/ButtonDefault';
 import {
     collection,
     getDocs,
@@ -10,38 +12,173 @@ import {
 } from "firebase/firestore";
 import { db, } from "../../firebase";
 
+// function Playground() {
+//     const [dataTotal, setDataTotal] = useState([]);
+
+//     useEffect(() => {
+//         getDataTotal();
+//     }, []);
+//     async function getDataTotal() {
+//         try {
+//             const ordersRef = collection(db, "count");
+//             const q = query(ordersRef, orderBy("total"));
+//             const querySnapshot = await getDocs(q);
+//             let data = [];
+//             querySnapshot.forEach((doc) => {
+//                 data.push({ ...doc.data(), id: doc.id });
+//             });
+//             setDataTotal(data);
+
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     }
+//     const totalSum = dataTotal.reduce((acc, data) => acc + data.total, 0,);
+
+//     return (
+//         <div className='p-10'>
+//             {dataTotal.map((data, i) => (
+//                 <>
+//                     <p key={i}>{data.total}</p>
+//                 </>
+//             ))}
+//             <p>Total Sum: {totalSum}</p>
+//         </div>
+//     )
+//}
+
+async function fetchMovies() {
+    const apiKey = '82754a7c6012d4a3b4654bdbbd3dbc41';  // Replace with your actual API key
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+
+    try {
+        const response = await axios.get(url);
+        return response.data.results; // Access the 'results' array of movies
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 function Playground() {
-    const [dataTotal, setDataTotal] = useState([]);
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const fetchedMovies = await fetchMovies();
+    //         setMovies(fetchedMovies);
+    //     };
+
+    //     fetchData();
+    // }, []);
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            const apiKey = '82754a7c6012d4a3b4654bdbbd3dbc41'; // Replace with your actual API key
+            const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`;
+
+            const response = await axios.get(url);
+            setSearchResults(response.data.results);
+        } catch (error) {
+            setError(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
 
     useEffect(() => {
-        getDataTotal();
+        const fetchMovies = async () => {
+            setIsLoading(true);
+            setError(null);
+
+            try {
+                const apiKey = '82754a7c6012d4a3b4654bdbbd3dbc41'; // Replace with your actual API key
+                const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+                const response = await axios.get(url);
+                const fetchedMovies = response.data.results;
+                setMovies(fetchedMovies);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchMovies();
     }, []);
-    async function getDataTotal() {
-        try {
-            const ordersRef = collection(db, "count");
-            const q = query(ordersRef, orderBy("total"));
-            const querySnapshot = await getDocs(q);
-            let data = [];
-            querySnapshot.forEach((doc) => {
-                data.push({ ...doc.data(), id: doc.id });
-            });
-            setDataTotal(data);
-
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    const totalSum = dataTotal.reduce((acc, data) => acc + data.total, 0,);
-
     return (
-        <div className='p-10'>
-            {dataTotal.map((data, i) => (
-                <>
-                    <p key={i}>{data.total}</p>
-                </>
-            ))}
-            <p>Total Sum: {totalSum}</p>
-        </div>
+        <>
+            <div className='p-10'>
+                <h1>Movie Search</h1>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Search for movies..."
+                        value={searchTerm}
+                        onChange={handleChange}
+                        className='text-green-500 border-b-[1px] border-blue-500 text-2xl w-full focus:outline-none focus:border-opacity-100 bg-transparent py-1'
+                    />
+                    <ButtonDefault
+                        text={'SEARCH'}
+                    />
+                </form>
+
+
+
+                {isLoading && <p>Loading results...</p>}
+                {error && <p>Error: {error.message}</p>}
+
+                {searchResults.length > 0 && (
+                    <div className='grid grid-cols-4 gap-5'>
+                        {searchResults.map((movie) => (
+                            <div key={movie.id}>
+                                <div className='flex gap-2 p-4 h-full items-center justify-center bgblurbluef border-2 border-blue-500'>
+                                    <div className='w-20 bg-blue-900'><img className='w-20'
+                                        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                                        alt={movie.title}
+                                    /></div>
+                                    <div className='w-52'>
+                                        <a href={`/movie/${movie.id}`} className='text-xl text-lime-500 line-clamp-1'>{movie.title}</a>
+                                        <p className='line-clamp-3 text-blue-500'>{movie.overview}</p>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+            {/* {isLoading && <p>Loading movies...</p>}
+            {error && <p>Error fetching movies: {error.message}</p>}
+            {movies.length > 0 && (
+                <ul>
+                    {movies.map((movie) => (
+                        <li key={movie.id}>
+                            <a href="#">{movie.title}</a>
+                            <p>{movie.id}</p>
+                            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                            <p>{movie.overview}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+            {movies.length === 0 && !isLoading && !error && <p>No movies found.</p>} */}
+        </>
     )
 }
 
